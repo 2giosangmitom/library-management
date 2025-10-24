1c581f2e5d3e528cc1c0482a7912394dba28aa7e
Vo Quang Chien <voquangchien.dev@proton.me>
2025-10-24 14:39:36 +0700

    feat(api): add prisma plugin

--- apps/api/src/plugins/prisma.ts
+++ apps/api/src/plugins/prisma.ts
@@ -1,5 +1,19 @@
 import { envType } from "@config/env-schema";
+import { PrismaClient } from "@prisma/client";
 
-export default async function (fastify: FastifyTypeBox, opts: envType) {
+export default async function (fastify: FastifyTypeBox, _opts: envType) {
   fastify.log.debug("Registering Prisma plugin");
+
+  const prisma = new PrismaClient();
+  fastify.decorate("prisma", prisma);
+
+  fastify.addHook("onClose", async () => {
+    await prisma.$disconnect();
+  });
+}
+
+declare module "fastify" {
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
 }
