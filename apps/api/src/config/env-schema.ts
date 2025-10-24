52326d957bbd0df8b69381726a01b87c8570e609
Vo Quang Chien <voquangchien.dev@proton.me>
2025-10-24 14:39:58 +0700

    feat(api): add redis plugin

--- apps/api/src/config/env-schema.ts
+++ apps/api/src/config/env-schema.ts
@@ -4,6 +4,7 @@ export const envSchema = Type.Object({
   CORS_ORIGINS: Type.Optional(Type.String()),
   CORS_METHODS: Type.Optional(Type.String()),
   DATABASE_URL: Type.String(),
+  REDIS_URL: Type.String(),
   JWT_SECRET: Type.String(),
 });
 
--- apps/api/src/plugins/redis.ts
+++ apps/api/src/plugins/redis.ts
@@ -1,5 +1,10 @@
 import { envType } from "@config/env-schema";
+import { fastifyRedis } from "@fastify/redis";
 
 export default async function (fastify: FastifyTypeBox, opts: envType) {
   fastify.log.debug("Registering Redis plugin");
+
+  await fastify.register(fastifyRedis, {
+    url: opts.REDIS_URL,
+  });
 }
