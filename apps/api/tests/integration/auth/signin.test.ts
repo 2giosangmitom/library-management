5308d7d12c9fabe96aa561974c78cc758a95a04a
Vo Quang Chien <voquangchien.dev@proton.me>
2025-10-25 20:09:38 +0700

    test(api/auth): add integration test for sign in feature

---
+++ apps/api/tests/integration/auth/signin.test.ts
@@ -0,0 +1,72 @@
+import { build } from '@tests/helpers/build';
+import * as hashUtils from '@utils/hash';
+
+describe('sign in', async () => {
+  const app = await build();
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeAll(async () => {
+    // Sign up an account for later usage
+    await app.inject({
+      method: 'POST',
+      path: '/auth/signup',
+      body: {
+        email: 'test-signin@test.com',
+        password: 'password123',
+        name: 'Test sign in'
+      }
+    });
+  });
+
+  it('should returns 401 if provide wrong email', async () => {
+    const response = await app.inject({
+      method: 'POST',
+      path: '/auth/signin',
+      body: {
+        email: 'test-signin-wrong@test.com',
+        password: 'password123'
+      }
+    });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.json()).toEqual({
+      message: 'Invalid email or password'
+    });
+  });
+
+  it('should returns 401 if provide wrong password', async () => {
+    const verifyHashSpy = vi.spyOn(hashUtils, 'verifyHash');
+
+    const response = await app.inject({
+      method: 'POST',
+      path: '/auth/signin',
+      body: {
+        email: 'test-signin@test.com',
+        password: 'wrong-password'
+      }
+    });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.json()).toEqual({
+      message: 'Invalid email or password'
+    });
+    expect(verifyHashSpy).toHaveBeenCalledOnce();
+  });
+
+  it('should return 200 status code with jwt if provide correct password', async () => {
+    const response = await app.inject({
+      method: 'POST',
+      path: '/auth/signin',
+      body: {
+        email: 'test-signin@test.com',
+        password: 'password123'
+      }
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json()).toHaveProperty('jwt');
+  });
+});
