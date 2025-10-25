35296230e2ac255c60f190f4841e125de465b04b
Vo Quang Chien <voquangchien.dev@proton.me>
2025-10-25 19:52:50 +0700

    test(api/auth): add unit test for sign in service

--- apps/api/tests/unit/modules/auth/service.test.ts
+++ apps/api/tests/unit/modules/auth/service.test.ts
@@ -72,4 +72,49 @@ describe('auth service', () => {
       ).rejects.toThrow('Database error');
     });
   });
+
+  describe('sign in', () => {
+    it('should return false if provide wrong email', async () => {
+      vi.spyOn(authModel, 'findUserByEmail').mockResolvedValueOnce(null);
+
+      await expect(authService.signIn({ email: 'test@example.com', password: 'password123' })).resolves.toEqual({
+        verifyResult: false,
+        user_id: null
+      });
+    });
+
+    it('should return false if provide wrong password', async () => {
+      vi.spyOn(authModel, 'findUserByEmail').mockResolvedValueOnce({
+        user_id: 'sample-uuid',
+        password_hash: 'mock-hash',
+        salt: 'mock-salt',
+        email: 'test@example.com',
+        name: 'Test',
+        role: 'MEMBER'
+      });
+      vi.spyOn(hashUtils, 'verifyHash').mockResolvedValueOnce(false);
+
+      await expect(authService.signIn({ email: 'test@example.com', password: 'password123' })).resolves.toEqual({
+        verifyResult: false,
+        user_id: null
+      });
+    });
+
+    it('should return true and user_id if password is correct', async () => {
+      vi.spyOn(authModel, 'findUserByEmail').mockResolvedValueOnce({
+        user_id: 'sample-uuid',
+        password_hash: 'mock-hash',
+        salt: 'mock-salt',
+        email: 'test@example.com',
+        name: 'Test',
+        role: 'MEMBER'
+      });
+      vi.spyOn(hashUtils, 'verifyHash').mockResolvedValueOnce(true);
+
+      await expect(authService.signIn({ email: 'test@example.com', password: 'password123' })).resolves.toEqual({
+        verifyResult: true,
+        user_id: 'sample-uuid'
+      });
+    });
+  });
 });
