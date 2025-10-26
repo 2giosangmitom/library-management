760d4a8002a7d8c167040124ec7e57667b3e1c93
Vo Quang Chien <voquangchien.dev@proton.me>
2025-10-26 09:45:11 +0700

    chore(api/middlewares): add comments

--- apps/api/src/middlewares/auth.ts
+++ apps/api/src/middlewares/auth.ts
@@ -1,9 +1,18 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 
+/**
+ * Authentication middleware to verify JWT tokens.
+ * @param req Fastify request object
+ */
 export async function authMiddleware(req: FastifyRequest) {
   await req.jwtVerify();
 }
 
+/**
+ * Middleware to check if the user has a 'LIBRARIAN' role.
+ * @param req Fastify request object
+ * @param reply Fastify reply object
+ */
 export async function isLibrarianMiddleware(req: FastifyRequest, reply: FastifyReply) {
   const data = req.user as JWTPayload;
   if (data.role !== 'LIBRARIAN') {
