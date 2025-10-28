84984d76999b65db93cee092ea2a068d4abba007
Vo Quang Chien <voquangchien.dev@proton.me>
2025-10-29 05:54:13 +0700

    feat(api): init hooks for all modules

---
+++ apps/api/src/modules/category/category.hooks.ts
@@ -0,0 +1,6 @@
+export default function categoryHooks(fastify: FastifyTypeBox) {
+  fastify.addHook('onRoute', (routeOptions) => {
+    routeOptions.schema = routeOptions.schema || {};
+    routeOptions.schema.tags = [...(routeOptions.schema.tags || []), 'Category'];
+  });
+}
