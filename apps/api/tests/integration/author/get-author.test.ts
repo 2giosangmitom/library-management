eb597e1e71de5bbefb96a93abe851062cac32a4d
Vo Quang Chien <voquangchien.dev@proton.me>
2025-10-30 21:52:36 +0700

    test(api): update integration test for get authors

--- apps/api/tests/integration/author/get-author.test.ts
+++ apps/api/tests/integration/author/get-author.test.ts
@@ -17,6 +17,58 @@ describe('get author', async () => {
       expect(response.statusCode).toBe(200);
       expect(Array.isArray(response.json())).toBe(true);
     });
+
+    it('should respect pagination parameters', async () => {
+      const jwt = app.jwt.sign(
+        {
+          user_id: 'librarian-uuid',
+          role: 'LIBRARIAN'
+        },
+        { expiresIn: '1h' }
+      );
+
+      // Ensure there are enough authors in the database
+      for (let i = 0; i < 15; i++) {
+        const createAuthorResponse = await app.inject({
+          method: 'POST',
+          path: '/author',
+          headers: {
+            Authorization: `Bearer ${jwt}`
+          },
+          body: {
+            name: `Author ${i}`,
+            biography: 'Some biography',
+            short_biography: 'Some short bio',
+            nationality: 'Some Nationality',
+            slug: `author-${i}`
+          }
+        });
+        expect(createAuthorResponse.statusCode).toBe(201);
+      }
+
+      const response = await app.inject({
+        method: 'GET',
+        path: '/author?page=1&limit=5'
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(Array.isArray(response.json())).toBe(true);
+      expect(response.json().length).toBe(5);
+
+      const responsePage2 = await app.inject({
+        method: 'GET',
+        path: '/author?page=2&limit=5'
+      });
+
+      expect(responsePage2.statusCode).toBe(200);
+      expect(Array.isArray(responsePage2.json())).toBe(true);
+      expect(responsePage2.json().length).toBe(5);
+
+      // Ensure that the authors on page 2 are different from page 1
+      for (const author of responsePage2.json()) {
+        expect(response.json()).not.toContainEqual(author);
+      }
+    });
   });
 
   describe('get author by slug', () => {
