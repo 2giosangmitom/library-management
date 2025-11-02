e652cf50b4def7d52e104cf2d75733cf7d28a4be
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-02 21:53:37 +0700

    feat(api): implement book management module with create book functionality

---
+++ apps/api/src/modules/book/book.service.ts
@@ -0,0 +1,36 @@
+import { BookModel } from './book.model';
+
+export class BookService {
+  private static instance: BookService;
+  private bookModel: BookModel;
+
+  private constructor(bookModel: BookModel) {
+    this.bookModel = bookModel;
+  }
+
+  public static getInstance(fastify: FastifyTypeBox, bookModel = BookModel.getInstance(fastify)): BookService {
+    if (!BookService.instance) {
+      BookService.instance = new BookService(bookModel);
+    }
+    return BookService.instance;
+  }
+
+  public createBook(data: {
+    title: string;
+    description: string;
+    total_copies: number;
+    available_copies?: number;
+    author_ids: string[];
+    category_ids: string[];
+  }) {
+    const available = data.available_copies ? data.available_copies : data.total_copies;
+    return this.bookModel.createBook({
+      title: data.title,
+      description: data.description,
+      total_copies: data.total_copies,
+      available_copies: available,
+      author_ids: data.author_ids,
+      category_ids: data.category_ids
+    });
+  }
+}
