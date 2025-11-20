c0bcc50dece1c50f8f52d238d6c19eac4dfc345b
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-20 14:56:41 +0700

    feat(sensible): add sharedSchemaId to fastifySensible registration

--- apps/api/src/plugins/sensible.ts
+++ apps/api/src/plugins/sensible.ts
@@ -5,7 +5,9 @@ export default fp(
   async (fastify: FastifyTypeBox) => {
     fastify.log.debug('Registering sensible plugin');
 
-    await fastify.register(fastifySensible);
+    await fastify.register(fastifySensible, {
+      sharedSchemaId: 'HttpError'
+    });
   },
   {
     name: 'Sensible'
