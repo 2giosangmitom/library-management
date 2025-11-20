bc12938e1f1e4073f55cce5b06693ab85478aa33
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-20 07:39:17 +0700

    feat(api): add autohooks for book_clone, location and publisher module

---
+++ apps/api/src/modules/book_clone/book_clone.hooks.ts
@@ -0,0 +1,7 @@
+export default function bookCloneHooks(fastify: FastifyTypeBox) {
+  fastify.addHook('onRoute', (routeOptions) => {
+    routeOptions.schema = routeOptions.schema || {};
+    routeOptions.schema.tags = [...(routeOptions.schema.tags || []), 'Book Clone'];
+    routeOptions.schema.security = [{ JWT: [] }];
+  });
+}
