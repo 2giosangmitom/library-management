078e7ae1462984a285479027a12889100dc2ecfc
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-23 14:59:02 +0700

    feat(api/admin): add auth and isAdmin hook

--- apps/api/src/modules/admin/autohooks.ts
+++ apps/api/src/modules/admin/autohooks.ts
@@ -1,6 +1,11 @@
+import { authHook, isAdminHook } from '@src/hooks/auth';
+
 export default function adminHooks(fastify: FastifyTypeBox) {
   fastify.addHook('onRoute', (routeOptions) => {
     routeOptions.schema = routeOptions.schema || {};
     routeOptions.schema.tags = [...(routeOptions.schema.tags || []), 'Admin'];
   });
+
+  fastify.addHook('preHandler', authHook);
+  fastify.addHook('preHandler', isAdminHook);
 }
