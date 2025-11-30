ddacc8a165c1643593aab19c12b9bad48e741fbe
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-30 15:02:32 +0700

    style: format code

--- apps/api/src/modules/book/autohooks.ts
+++ apps/api/src/modules/book/autohooks.ts
@@ -1,4 +1,4 @@
-import { addRouteTags } from "@hooks/onRoute";
+import { addRouteTags } from '@hooks/onRoute';
 
 export default function bookHooks(fastify: FastifyTypeBox) {
   fastify.addHook('onRoute', addRouteTags('Book'));
