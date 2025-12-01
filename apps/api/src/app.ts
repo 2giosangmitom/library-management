1ce0d27f3d1a9b98fc64ad936fd5c0b135a77ca0
Vo Quang Chien <voquangchien.dev@proton.me>
2025-12-01 15:08:47 +0700

    chore(api): update logging options

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -11,12 +11,7 @@ export async function buildApp() {
         process.env.NODE_ENV === 'production'
           ? undefined
           : {
-              target: 'pino-pretty',
-              options: {
-                colorize: true,
-                translateTime: 'SYS:standard',
-                ignore: 'pid,hostname'
-              }
+              target: 'pino-pretty'
             }
     }
   })
